42705f28d48143bbde87a3a615c0aa96ef921cc6
Matt Levy <milevy1@gmail.com>
2019-05-13 09:14:36 -0600

    Added spec for user to search for alt fuel stations

---
+++ spec/features/user_can_search_alt_fuel_stations_nearby_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature "User can search alt fuel stations nearby" do
+  scenario "and view navbar contents" do
+    # As a user
+    # When I visit "/"
+    visit "/"
+
+    # And I fill in the search form with 80206 (Note: Use the existing search form)
+    # And I click "Locate"
+    within(".navbar") do
+      fill_in 'q', with: '80206'
+      click_on 'Locate'
+    end
+
+    # Then I should be on page "/search"
+    expect(current_path).to eq('/search')
+
+    # Then I should see the total results of the stations that match my query, 90.
+    expect(page).to have_content('Total results: 90')
+
+    # Then I should see a list of the 15 closest stations within 5 miles sorted by distance
+    expect(page).to have_css('.nearby-station', count: 15)
+
+    # And the stations should be limited to Electric and Propane
+    # And the stations should only be public, and not private, planned or temporarily unavailable.
+    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+  end
+end
+
+# You will be using this documentation: https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/nearest/
