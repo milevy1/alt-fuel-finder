b1feb4656415d880427b54c398fc2877de4231a9
Matt Levy <milevy1@gmail.com>
2019-05-13 10:50:35 -0600

    Refactored API consumption to nrel_service

--- app/models/stations_facade.rb
+++ app/models/stations_facade.rb
@@ -4,29 +4,26 @@ class StationsFacade
   end
 
   def total_results
-    JSON.parse(response.body, symbolize_names: true)[:total_results]
+    nrel_total_results
   end
 
   def stations
-    nearest_15_stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations].take(15)
-
-    nearest_15_stations.map do |station_data|
+    nrel_nearby_stations.map do |station_data|
       Station.new(station_data)
     end
   end
 
-  def conn
-    Faraday.new('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json') do |f|
-      f.adapter Faraday.default_adapter
-      f.params['api_key'] = ENV['nrel_api_key']
-      f.params['location'] = @zip_code
-      f.params['radius'] = 5.0
-      f.params['fuel_type'] = 'ELEC,LPG'
-      f.params['access'] = 'public'
-    end
+  private
+
+  def nrel_service
+    @nrel_service ||= NrelService.new(@zip_code)
   end
 
-  def response
-    conn.get
+  def nrel_total_results
+    @nrel_total_results ||= nrel_service.retrieve(:total_results)
+  end
+
+  def nrel_nearby_stations
+    @nrel_nearby_stations ||= nrel_service.retrieve(:fuel_stations, 15)
   end
 end
